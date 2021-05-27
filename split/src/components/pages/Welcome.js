44e111422249f24d03b17285f95628e28f8c8164
Abdul Uddin <auddin431@gmail.com>
2021-05-27 18:00:25 -0400

    some styling?

--- split/src/components/pages/Welcome.js
+++ split/src/components/pages/Welcome.js
@@ -1,42 +1,48 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import "./Welcome.css";
+
+const StyledButton = withStyles({
+  root: {
+    background: "black",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    "&:active": {
+      backgroundColor: "black",
+      color: "white",
+    },
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+  hover: {
+    background: "white",
+  },
+})(Button);
 
 const Welcome = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  };
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
   return (
-    <div>
+    <div className="wrapper">
       <h1>Welcome to Split.</h1>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
+      <form noValidate autoComplete="off">
+        <TextField id="email" label="Email" onChange={emailHandler} />
+        <TextField id="pass" label="Password" onChange={passwordHandler} />
+      </form>
+      <StyledButton variant="contained">Log In</StyledButton>
     </div>
   );
 };
