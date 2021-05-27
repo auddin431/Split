6042aef004f07b8dd4ea79775f28f01b7d3e666f
Abhinav Modugula <abhimodugula@gmail.com>
2021-05-27 17:31:31 -0400

    added basic backend functions - need to test

--- backend/split_backend/auth.py
+++ backend/split_backend/auth.py
@@ -1,7 +1,7 @@
 """
 Handles routes for user registration and authentication
 """
-
+import functools
 from .models import db, User
 from flask import Blueprint, session, g, request
 
@@ -9,13 +9,51 @@ auth_bp = Blueprint(
     "auth_bp", __name__
 )
 
-@auth_bp.route('/split_api/user/create', methods=["GET"])
+@auth_bp.route("/split_api/logout", methods=["POST"])
+def api_logout():
+    session.clear()
+    return 1
+
+@auth_bp.route("/split_api/user/login", methods=["POST"])
+def api_login():
+    params = request.args
+    username = params.get("username")
+    passwd = params.get("password")
+    user = User.query.filter_by(name=username).first()
+    if user and user.check_password(password=passwd):
+        session.clear()
+        session["user_id"] = user.id
+        return user.id
+    else:
+        return 0
+
+@auth_bp.route('/split_api/user/create', methods=["POST"])
 def api_reg_user():
     params = request.args
-    username = params.get("name")
-    passwd = params.get("passwd")
-    #TODO: Create user
+    username = params.get("username")
+    passwd = params.get("password")
+
+    existing_user = User.query.filter_by(name=username).first()
+
+    if existing_user is not None:
+        #error, username already in use
+        return 0
+    else:
+        new_user = User(name=username, need_to_pay=0, owed=0)
+        new_user.set_password(passwd)
+        db.session.add(new_user)
+        db.session.commit()
+        session.clear()
+        session["user_id"] = new_user.id
 
     return 1 #success
 
+@auth_bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.filter_by(id=user_id).first()
 
--- backend/split_backend/routes.py
+++ backend/split_backend/routes.py
@@ -1,7 +1,118 @@
-from .models import db, User, Purchase, Item
+from .models import db, User, Purchase, Item, Group
 from flask import Blueprint, g, session, request
 
 api_bp = Blueprint(
     "api_bp", __name__
 )
 
+#create group and returns the group id
+#also creates an empty purchase object
+@api_bp.route("/split_api/create_group", methods=["POST"])
+def create_group():
+    #need to pass in group name and purchase name
+    #group_name, purchase_name
+    new_group = Group(name=request.args.get("group_name"))
+    new_purchase = Purchase(name=request.args.get("purchase_name"))
+    new_purchase.group_id = new_group.id
+    new_purchase.group = new_group
+    db.session.add(new_purchase)
+    db.session.add(new_group)
+    db.session.commit()
+    return new_group.id
+
+
+#add user to group
+@api_bp.route("/split_api/add_user_to_group", methods=["POST"])
+def add_user_to_group():
+    """
+    Need to pass in user_id, and group_id
+    :return: group id is success, or -1
+    """
+    params = request.args
+    user_id = params.get("user_id")
+    group_id = params.get("group_id")
+    user = User.query.filter_by(name=user_id).first()
+    group = Group.query.filter_by(id=group_id)
+    user.group_id = group_id
+    user.group = group
+    db.session.commit()
+    return group_id
+
+
+#add item reuqest for a user
+@api_bp.route("/split_api/add_item", methods=["POST"])
+def req_item():
+    #takes in user id, item name, and amount to add for user
+    #will create item if doesn't exist yet
+    #user_id, item_name, amount
+    #returns item id
+    params = request.args
+    user_id = params.get("user_id")
+    item_name = params.get("item_name")
+    amount = params.get("amount")
+
+    item = Item.query.filter_by(name=item_name).first()
+    if item is None:
+        item = Item(name=item_name, count=0)
+    item.count += amount
+
+    user = User.query.filter_by(id=user_id).first()
+    item.user_id = user_id
+    item.user = user
+    return item.id
+
+#subtract item reuqest for user
+@api_bp.route("/split_api/add_item", methods=["POST"])
+def sub_item():
+    #takes in user id, item name, and amount to subtract for user
+    #user_id, item_name, amount
+    #returns item id
+    #does nothing if item does not exist
+    params = request.args
+    user_id = params.get("user_id")
+    item_name = params.get("item_name")
+    amount = params.get("amount")
+
+    item = Item.query.filter_by(name=item_name).first()
+    if item is None:
+        return -1
+    item.count -= amount
+
+    user = User.query.filter_by(id=user_id).first()
+    item.user_id = user_id
+    item.user = user
+    return item.id
+
+#get money other people oe a user
+@api_bp.route("/split_api/get_owed_money", methods=["POST"])
+def get_money_owed():
+    params = request.args
+    user_id = params.get("user_id")
+
+    user = User.query.filter_by(id=user_id).first()
+    return user.owed
+
+#get money user owes another
+@api_bp.route("/split_api/get_money_owes_others", methods=["POST"])
+def get_money_owes_others():
+    params = request.args
+    user_id = params.get("user_id")
+
+    user = User.query.filter_by(id=user_id).first()
+    return user.need_to_pay
+
+#return dict of items {item: {user: count, user2: count2}}
+@api_bp.route("/split_api/item_list"):
+def item_list():
+    """
+    Main method to display the item list along with the amount each user
+    reuqests. This is in the format {item: {user: count, user2: count2}}
+    amount each user
+    :param: group id
+    :return: the JSON dict descirbed above
+    """
+    pass
+
+
+#calculate split - scans the reciept, gets items costs, assigns to users
+#takes in an image parameter, and user
