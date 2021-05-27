69bb917b2985a6d82af929b3ae970ee1575b7c3e
Abdul Uddin <auddin431@gmail.com>
2021-05-27 16:14:27 -0400

    setup router

--- split/src/index.js
+++ split/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Welcome from "./components/pages/Welcome";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Switch>
+      <Route path="/about">
+        <App />
+      </Route>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
