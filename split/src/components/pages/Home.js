97d258c45d4b72454ee0128e52908aeac3a1a3d2
Abdul Uddin <auddin431@gmail.com>
2021-05-27 23:32:35 -0400

    owe component created

--- split/src/components/pages/Home.js
+++ split/src/components/pages/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Owe from "../Owe";
+import { Helmet } from "react-helmet";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Hello world</div>;
+  return (
+    <React.Fragment>
+      <Helmet>
+        <style>{"body { background-color: black; }"}</style>
+      </Helmet>
+      <div className="wrapperBackground">
+        <Owe owe="128.37" owed="40.59" />
+      </div>
+      <div
+        style={{
+          backgroundColor: "white",
+          borderRadius: "25px",
+          height: "70vh",
+        }}
+      ></div>
+    </React.Fragment>
+  );
 };
 
 export default Home;
